a492c1397de633a27f03a4b26eee49fa833dbb59
Александр Шаповалов <ashsvis@gmail.com>
2022-05-16 18:23:30 +0300

    Фиксация срипта создания бд

--- drycleaner.sql
+++ drycleaner.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `drycleaner` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `maternityhospital`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `appointments` (
